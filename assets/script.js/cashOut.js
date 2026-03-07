c8ca1456c19e99347eaf8af4c8fcc6b1db2ce3bd
Abir <grabirahmed@gmail.com>
2026-03-08 00:08:09 +0600

    cashOut desihn and code

---
+++ assets/script.js/cashOut.js
@@ -0,0 +1,27 @@
+document.getElementById("cashout-btn").addEventListener("click", function () {
+  // get the agent number & validate
+  const cashoutNumber = getValueFromInput("cashout-number");
+  if (cashoutNumber.length != 11) {
+    alert("invalid number");
+    return;
+  }
+  //get the amount
+  const cashoutAmount = getValueFromInput("cashout-amount");
+
+  const currentBalance = getBalance();
+  //calculate Balance
+  const newBalance = currentBalance - Number(cashoutAmount);
+  console.log(newBalance);
+  if (newBalance < 0) {
+    alert("invalid Amount");
+    return;
+  }
+  const pin = getValueFromInput("cashout-pin");
+  if (pin === "1234") {
+    alert("Cashout Successfull");
+    setBalance(newBalance);
+  } else {
+    alert("invalid Pin");
+    return;
+  }
+});
